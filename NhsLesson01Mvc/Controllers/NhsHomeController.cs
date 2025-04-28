3bd3279d373c4bc80904e46004e5dcaa11c630a5
Nguyen Hai Son <sonnguyennr111@gmail.com>
2025-04-28 22:24:25 +0700

    Them thu muc NhsLesson01

---
+++ NhsLesson01Mvc/Controllers/NhsHomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NhsLesson01Mvc.Models;
+
+namespace NhsLesson01Mvc.Controllers
+{
+    public class NhsHomeController : Controller
+    {
+        private readonly ILogger<NhsHomeController> _logger;
+
+        public NhsHomeController(ILogger<NhsHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("NhsIndex"); // Đảm bảo gọi đúng file view
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
