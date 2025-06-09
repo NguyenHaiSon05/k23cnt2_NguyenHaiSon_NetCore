548c40716fe0eb72d5c74811b87f7ef5fc46e436
Nguyen Hai Son <sonnguyennr111@gmail.com>
2025-06-09 21:43:09 +0700

    Them thu muc

--- NhsLesson07/obj/Debug/net8.0/NhsLesson07.AssemblyInfo.cs
+++ NhsLesson07/obj/Debug/net8.0/NhsLesson07.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NhsLesson07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd3279d373c4bc80904e46004e5dcaa11c630a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5276a7406605aac572f7082c3b9469515039535d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NhsLesson07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NhsLesson07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
